4dea4768eccc8a4d0a9b074811945fadf5d45927
Abhishek Kumar <60790645+Abhi23raj472@users.noreply.github.com>
2020-06-26 09:08:33 +0530

    Simple Interest using user input

---
+++ Simple_Interest.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int p,r,t;
+	float si;
+	cout<<"P :";
+	cin>>p;
+	cout<<"R :";
+	cin>>r;
+	cout<<"T :";
+	cin>>r;
+	si=p*r*t/100.0;
+	cout<<"Simple Interest ="<<si<<endl;
+	return 0;
+}
