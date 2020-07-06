be560ea2d255d08fd3bb6d55a2bdf1fdd8a4f748
Abhishek Kumar <60790645+Abhi23raj472@users.noreply.github.com>
2020-07-06 07:54:45 +0530

    Pattern problem- 0's and 1's

---
+++ 0's_and_1's.cpp
@@ -0,0 +1,43 @@
+/*Pattern problem
+N=6
+--For ith line print i numbers
+--odd line starts with 0,and value alternate between 0 and 1
+--even line starts with 1,and value alternate between 1 and 1
+--print n lines
+
+0
+10
+010
+1010
+01010
+101010
+*/
+
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		int val=i%2==0 ? 1:0;
+		
+		//print i value in ith line
+		
+		for(int cnt=1;cnt<=i;cnt++){
+			cout<<val;
+			val=1-val;
+		}
+		cout<<endl;
+	}
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+}
