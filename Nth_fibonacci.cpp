7d4b35316063d55d0e1ef254ea6972839da44080
Abhishek Kumar <60790645+Abhi23raj472@users.noreply.github.com>
2020-07-08 09:44:43 +0530

    Nth fibonacci

---
+++ Nth_fibonacci.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+/*Fibonacci series- Nth fibonacci Number
+0,1,1,2,3,5,8,,13,....
+*/
+int fibonacci(int n){
+	int a=0;
+	int b=1;
+	int c;
+	for(int i=1;i<=n-1;i++){
+		c=a+b;
+		a=b;
+		b=c;
+	}
+	return c;
+}
+
+
+
+int main(){
+	int n;
+	cin>>n;
+	cout<<fibonacci(n)<<endl;
+	
+	return 0;
+}
