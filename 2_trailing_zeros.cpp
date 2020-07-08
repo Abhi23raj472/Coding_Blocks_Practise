019152a8ca5ec4d6b63e0bb5cd5a1734258613a3
Abhishek Kumar <60790645+Abhi23raj472@users.noreply.github.com>
2020-07-08 09:43:31 +0530

    Add files via upload

---
+++ 2_trailing_zeros.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int findZeros(int n){
+	int ans=0;
+	for(int D=5;n/D>=1;D=D*5){
+		ans+=n/D;
+	}
+	return ans;
+}
+int main(){
+	long long int n;
+	cin>>n;
+	cout<<findZeros(n)<<endl;
+	return 0;
+}
